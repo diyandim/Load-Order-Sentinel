6fb1d5c0eb98cf59593dae4c8843b3a681a9cf32
Diyan <diyanmardimitrov@gmail.com>
2026-08-17 13:40:58 +0300

    Added an empty list that collects the plugins inside

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
 def main():
     f = open("plugins.txt", "r")
+    collected_plugins = []
     for plugin in f:
-        print(plugin.strip())
+        collected_plugins.append(plugin.strip())
     f.close()
+    print(collected_plugins)
+    return collected_plugins
+    
+
 if __name__ == "__main__":
     main()
+
