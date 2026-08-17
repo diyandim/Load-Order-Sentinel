286a65f2665b3a01c41b7c1985dc2cf8ba837ae0
diyandim <diyanmardimitrov@gmail.com>
2026-08-17 22:27:41 +0300

    Add pytest test for ConflictRule

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import Plugin, DuplicateRule
+from main import Plugin, DuplicateRule, ConflictRule
 
 def test_finds_duplicate_plugin():
     plugin_one = Plugin("SomeName.esp", True)
@@ -6,3 +6,15 @@ def test_finds_duplicate_plugin():
     test_plugins = [plugin_one, plugin_two]
     result = DuplicateRule().check(test_plugins)
     assert "SomeName.esp" in result
+
+def test_conflict_rule_plugin():
+    plugin_one = Plugin("Ordinator.esp", True)
+    plugin_two = Plugin("SkyrimUI.esp", True)
+    plugin_three = Plugin("ImmersiveArmors.esp", True)
+    plugin_four = Plugin("Campfire.esp", True)
+    test_plugins = [plugin_one, plugin_two, plugin_three, plugin_four]
+    result = ConflictRule().check(test_plugins)
+    assert "Ordinator.esp: Conflicts with Apocalypse." in result
+    assert "SkyrimUI.esp: Conflicts with AnotherMod." in result
+    assert "Campfire.esp: Conflicts with Frostfall." in result
+    assert "ImmersiveArmors.esp" not in str(result)
