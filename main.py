fe6135e6f1869375a435c97ef455509f0aa99211
Diyan <diyanmardimitrov@gmail.com>
2026-08-17 14:13:49 +0300

    Parse plugins.txt into Plugin objects

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
+class Plugin():
+    def __init__(self, name: str, enabled: bool):
+        self.name = name
+        self.enabled = enabled
+
 def main():
     f = open("plugins.txt", "r")
     collected_plugins = []
+
     for plugin in f:
-        collected_plugins.append(plugin.strip())
+        stripped = plugin.strip()
+        is_enabled = stripped.startswith("*")
+        clean_name = stripped.strip("*")
+        collected_plugins.append(Plugin(clean_name, is_enabled))
+
     f.close()
     print(collected_plugins)
     return collected_plugins
-    
+
 
 if __name__ == "__main__":
     main()
-
